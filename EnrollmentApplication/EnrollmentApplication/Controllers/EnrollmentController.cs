13fcdef13dcacdab558a6ef353607995f278b1a0
RyanWilliams1 <s00602709@acad.tri-c.edu>
2019-02-17 20:45:53 -0500

    Lab 6 Final

--- EnrollmentApplication/EnrollmentApplication/Controllers/EnrollmentController.cs
+++ EnrollmentApplication/EnrollmentApplication/Controllers/EnrollmentController.cs
@@ -41,6 +41,7 @@ namespace EnrollmentApplication.Controllers
         {
             ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "CourseTitle");
             ViewBag.StudentID = new SelectList(db.Students, "StudentID", "StudentLastName");
+
             return View();
         }
 
@@ -49,7 +50,7 @@ namespace EnrollmentApplication.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "EnrollmentID,StudentID,CourseID,Grade")] Enrollment enrollment)
+        public ActionResult Create([Bind(Include = "EnrollmentID,StudentID,CourseID,Grade,IsActive,AssignedCampus,EnrollmentSemester,EnrollmentYear")] Enrollment enrollment)
         {
             if (ModelState.IsValid)
             {
@@ -85,7 +86,7 @@ namespace EnrollmentApplication.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "EnrollmentID,StudentID,CourseID,Grade")] Enrollment enrollment)
+        public ActionResult Edit([Bind(Include = "EnrollmentID,StudentID,CourseID,Grade,IsActive,AssignedCampus,EnrollmentSemester,EnrollmentYear")] Enrollment enrollment)
         {
             if (ModelState.IsValid)
             {
