d04f8af3cc5b4fa5c4917815077bfde4854dbf01
RyanWilliams1 <s00602709@acad.tri-c.edu>
2019-03-17 12:42:55 -0400

    Lab 8 Final

---
+++ RyansAuthIndividualAccounts/RyansAuthIndividualAccounts/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RyansAuthIndividualAccounts.Startup))]
+namespace RyansAuthIndividualAccounts
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
