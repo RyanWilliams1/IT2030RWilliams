bb7e64821beb5e8b4ddadb0b3b2c3ac0677b0b6a
RyanWilliams1 <s00602709@acad.tri-c.edu>
2019-03-31 19:45:09 -0400

    Final Commit Lab 10

---
+++ MVCMusicStoreApplication/MVCMusicStoreApplication/Controllers/StoreController.cs
@@ -0,0 +1,40 @@
+﻿using MVCMusicStoreApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+
+namespace MVCMusicStoreApplication.Controllers
+{
+    public class StoreController : Controller
+    {
+        private MVCMusicStoreDBContext db = new MVCMusicStoreDBContext();
+
+        // GET: Store
+        public ActionResult Index(int id)
+        {
+            var albums =
+                from album in db.Albums
+                where album.GenreId == id
+                select album;
+
+            return View(albums.ToList());
+            
+        }
+
+        public ActionResult Browse()
+        {
+            return View(db.Genres.ToList());
+        }
+
+        public ActionResult Details(int id)
+        {
+            Album album = db.Albums.Find(id);
+            
+            return View(album);
+        }
+    }
+}
