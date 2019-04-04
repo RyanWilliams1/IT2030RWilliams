152c386c08bf630c22f8f98b70188b2f1e3b0614
RyanWilliams1 <s00602709@acad.tri-c.edu>
2019-04-04 19:11:51 -0400

    Lab 11 inital

---
+++ MVCMusicStoreApplication/MVCMusicStoreApplication/Controllers/ShoppingCartController.cs
@@ -0,0 +1,30 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMusicStoreApplication.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        // GET: ShoppingCart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: ShoppingCart/AddToCart
+        public ActionResult AddToCart()
+        {
+            throw new NotImplementedException();
+        }
+
+        //POST: ShoppingCart/RemoveFromCart
+        [HttpPost]
+        public ActionResult RemoveFromCart()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
